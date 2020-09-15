e32b15d92ab06584c1ca5d4ea41d162c95121709
vidushi <haksarvidushi@gmail.com>
2020-09-15 12:53:14 +0530

    added sticky,css and logic for move sticky

--- tools.js
+++ tools.js
@@ -3,33 +3,59 @@ ctx.lineWidth = 5;
 ctx.lineCap = "round";
 ctx.lineJoin = 'round';
 let activeTool = 'pencil';
+ctx.imageSmoothingEnabled = true;
+ctx.imageSmoothingQuality = "high";
 let pencil = document.querySelector("#pencil");
 let eraser = document.querySelector("#eraser");
+let sticky = document.querySelector("#sticky");
 let pencilOptions = document.querySelector("#pencil-options");
 let eraserOptions = document.querySelector("#eraser-options");
+let toggleP = false;
+let toggleE = false;
+
 
 function handleTool(tool) {
     if (tool == "pencil") {
         ctx.strokeStyle = "black";
-        if (activeTool == "pencil") {
-            pencilOptions.classList.add("show");
-        } else {
+        // if (activeTool == "pencil" && toggleP == false) {
+        //     pencilOptions.classList.add("show");
+        //     toggleP = true;
+        // } else 
+        if (activeTool == "pencil" && toggleP == true) {
             ctx.strokeStyle = "black";
             activeTool = "pencil";
+            toggleP = false;
+            pencilOptions.classList.remove("show");
+        } else {
+            activeTool = "pencil";
+            toggleP = true;
+            pencilOptions.classList.add("show");
             eraserOptions.classList.remove("show");
+
         }
     } else if (tool == "eraser") {
         ctx.strokeStyle = "white"
-        if (activeTool == "eraser") {
-            eraserOptions.classList.add("show");
-        } else {
+            // if (activeTool == "eraser" && toggleE == false) {
+            //     eraserOptions.classList.add("show");
+            //     toggleE = true;
+            //} else
+        if (activeTool == "eraser" && toggleE == false) {
             ctx.strokeStyle = "white";
             activeTool = "eraser";
-            pencilOptions.classList.remove("show");
-        }
+            toggleE = true;
+            eraserOptions.classList.remove("show");
 
+        } else {
+            activeTool = "eraser";
+            toggleE = false;
+            eraserOptions.classList.add("show");
+            pencilOptions.classList.remove("show");
+
+
+        }
     } else if (tool == "sticky") {
         createSticky();
+
     }
 }
 
@@ -43,4 +69,23 @@ for (let i = 0; i < sliders.length; i++) {
         let width = sliders[i].value;
         ctx.lineWidth = width;
     })
+}
+
+const hamburger = document.querySelector(".hamburger");
+const toolPanel = document.querySelector(".tool-panel");
+hamburger.addEventListener("click", function() {
+    handleHamburger();
+})
+let isActive = true;
+
+function handleHamburger() {
+    if (isActive == true) {
+        hamburger.classList.remove("is-active");
+        toolPanel.classList.remove("add-animation");
+    } else {
+        hamburger.classList.add("is-active");
+        toolPanel.classList.add("add-animation");
+    }
+
+    isActive = !isActive;
 }
